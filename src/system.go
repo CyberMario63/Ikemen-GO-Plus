bc0e6a2432df0053b28376338845f1b62fd82ac9
ShinLucho <shinlucho@gmail.com>
2018-01-12 12:33:42 -0300

    Added mp3 decoder dependency
    mp3 isn't supported yet

--- src/system.go
+++ src/system.go
@@ -33,7 +33,7 @@ var sys = System{
 	lifeMul:    1, team1VS2Life: 1,
 	turnsRecoveryRate: 1.0 / 300,
 	mixer:             *newMixer(),
-	bgm:               *newVorbis(),
+	bgm:               *newBGM(),
 	sounds:            newSounds(16),
 	allPalFX:          *newPalFX(),
 	bgPalFX:           *newPalFX(),
@@ -83,7 +83,7 @@ type System struct {
 	debugScript             string
 	debugDraw               bool
 	mixer                   Mixer
-	bgm                     Vorbis
+	bgm                     BGM
 	audioContext            *openal.Context
 	nullSndBuf              [audioOutLen * 2]int16
 	sounds                  Sounds
